eae6c60dfac1e143024260099534c74962207f0d
SharMelde <njugunashar26@gmail.com>
2025-10-12 18:21:30 +0300

    Fix CORS for GitHub Pages frontend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:3000",
       "https://ccsharon26.github.io", 
       "https://ccsharon26.github.io/CutWaterz", 
     ],
@@ -28,6 +28,14 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 
 console.log("Server running with:", {
